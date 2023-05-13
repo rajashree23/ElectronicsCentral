60d04b5b4114fba8d9ed7bdc9bb88d7e19797d8b
Rajashree Parhi <rajashree.parhi@factset.com>
2023-05-13 22:49:29 +0530

    feat: add mobile view of navbar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faSearch,
   faShoppingCart,
   faHeart,
+  faUser,
+  faXmark
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
@@ -11,24 +14,72 @@ import "./mobile.layout.css";
 import "./desktop.layout.css";
 
 export const Navbar = () => {
+  const [hamburgerSelected, setHamburgerSelected] = useState(false);
+
   return (
-    <nav className="nav-container">
-      <div className="image-container">
-        <Link to="/">
-          <img src={Logo} alt="logo" />
-        </Link>
-      </div>
+    <>
+      <nav className="nav-container">
+        <div>
+          <Link to="/">
+            <img src={Logo} alt="logo" />
+          </Link>
+        </div>
 
-      <div className="input-container">
-        <FontAwesomeIcon icon={faSearch} className="search-icon" />
-        <input placeholder="Search" />
-      </div>
+        <div className="input-container">
+          <FontAwesomeIcon icon={faSearch} className="search-icon" />
+          <input placeholder="Search" />
+        </div>
 
-      <div className="action-items">
-        <FontAwesomeIcon icon={faShoppingCart} className="icon" />
-        <FontAwesomeIcon icon={faHeart} className="icon" />
-        <button>Login</button>
+        <div className="action-items">
+          <FontAwesomeIcon icon={faShoppingCart} className="icon" />
+          <FontAwesomeIcon icon={faHeart} className="icon" />
+          <Link to="/login" className="link">
+            Login
+          </Link>
+        </div>
+      </nav>
+
+      <nav className="mobile-nav-container">
+        <div className="image-container">
+          <Link to="/">
+            <img src={Logo} alt="logo" />
+          </Link>
+        </div>
+
+        <div
+          className="hamburger"
+          onClick={() => setHamburgerSelected((prev) => !prev)}
+        >
+          {hamburgerSelected ? (
+              <FontAwesomeIcon icon={faXmark} className="icon cross" />
+          ) : (
+            <>
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </>
+          )}
+        </div>
+      </nav>
+
+      <div className={`${hamburgerSelected ?"nav-items":"none" }`}>
+        <div className="row"   onClick={() => setHamburgerSelected((prev) => !prev)}>
+          <FontAwesomeIcon icon={faShoppingCart} className="icon " />
+          <p>Cart</p>
+        </div>
+
+        <div className="row"   onClick={() => setHamburgerSelected((prev) => !prev)}>
+          <FontAwesomeIcon icon={faHeart} className="icon" />
+          <p>Wishlist</p>
+        </div>
+
+        <div className="row"   onClick={() => setHamburgerSelected((prev) => !prev)}>
+          <FontAwesomeIcon icon={faUser} className="icon" />
+          <Link to="/login" className="link">
+            Login
+          </Link>
+        </div>
       </div>
-    </nav>
+    </>
   );
 };
